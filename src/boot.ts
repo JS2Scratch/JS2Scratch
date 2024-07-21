ed1589d31b8a4fa567f9938d9db2261a85e6be60
ShadowX <105983456+ShadowsIndeedWhisper@users.noreply.github.com>
2024-07-21 08:17:40 +0100

    new cmd

--- src/boot.ts
+++ src/boot.ts
@@ -13,6 +13,7 @@ import { new_args } from './lib/arg'
 import { argv } from 'process'
 import { errorMessages } from './lib/console'
 import { start_env } from './environment/env'
+import { DirectoryBuffer, FileBuffer } from './lib/fs';
 
 let subCommand = argv[2];
 
@@ -25,7 +26,7 @@ if (!subCommand) {
 let argumentsCollected = new_args([
     "-i", "-input",
     "-o", "-out", "-output",
-    "-t", "-turbo", "-turbowarp"
+    "-t", "-turbo", "-turbowarp",
 ], [
     ["-i", "-input"],
     ["-o", "-out", "-output"],
@@ -37,6 +38,24 @@ switch (subCommand) {
         start_env(argumentsCollected);
         break;
 
+    case "new":
+        let newDir = new DirectoryBuffer("Project");
+        let spriteDir = new DirectoryBuffer("Sprite1.sprite");
+        let spriteMain = new FileBuffer("main.js", "");
+        let projectJson = new FileBuffer("project.d.json", `{
+    "Sprite1": {
+        "Type": "Sprite",
+        "Costumes": [],
+        "Sounds": []
+    }
+}`)
+        spriteDir.Append([spriteMain]);
+        newDir.Append([projectJson, spriteDir]);
+
+        if (argumentsCollected["-i"] == "null") argumentsCollected["-i"] = "";
+        newDir.Instantiate(argumentsCollected["-i"]);
+        break;
+        
     default:
         errorMessages['Unknown parameter'](subCommand);
 }
